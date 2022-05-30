d4acef9dc92ad88532b16d4533517b4d0e37e74c
weimar <weimarbfz@gmail.com>
2022-05-30 11:49:43 -0500

    Hallar perimetro de un cuadrado

---
+++ perimetroCuadrado.js
@@ -0,0 +1,8 @@
+const hallarPerimetroCuadrado = (lado) => {
+  return perimetro = lado * 4;
+   
+}
+
+perimetro= prompt("ingrese el lado del perimetro");
+
+console.log(`El perimetro del cuadrado es: ${hallarPerimetroCuadrado(perimetro)}`);
