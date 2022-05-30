30c8a27ce29029d1e80c8ad20fb403c6d888c74b
weimar <weimarbfz@gmail.com>
2022-05-30 12:59:38 -0500

    switch, func Perimetro y area de un cuadrado

--- perimetroCuadrado.js
+++ perimetroCuadrado.js
@@ -1,8 +1,36 @@
-const hallarPerimetroCuadrado = (lado) => {
-  return perimetro = lado * 4;
-   
+let opc = prompt("Ingrese opción a calcular");
+
+switch (opc) {
+  case '1':
+    perimetro = prompt("ingrese el lado del perimetro");
+
+    const hallarPerimetroCuadrado = (lado) => {
+      return perimetro = lado * 4;
+
+    }
+
+    console.log(`El perimetro del cuadrado es: ${hallarPerimetroCuadrado(perimetro)}`);
+
+    break;
+  case '2':
+    area = prompt("Ingrese lado del cuadrado");
+    
+    const hallarAreaCuadrado = (lado) => {
+      return area = lado * lado;
+       
+    }
+
+     console.log(`El area del cuadrado es: ${hallarAreaCuadrado(area)}`);
+    
+    break;
+
+  default:
 }
 
-perimetro= prompt("ingrese el lado del perimetro");
 
-console.log(`El perimetro del cuadrado es: ${hallarPerimetroCuadrado(perimetro)}`);
+
+
+
+
+
+
